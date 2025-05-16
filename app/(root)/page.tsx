a8a4c60bec1cf58b236c82f9058f8d033ce1421c
elmurod <elmurodvkh@gmail.com>
2025-05-16 23:50:23 +0500

    home page ui #part_1

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+    <main className="wrapper page">
+      <Header
+        title="All Videos"
+        subHeader="Public Library"
+      />
+    </main>
+  )
+}
